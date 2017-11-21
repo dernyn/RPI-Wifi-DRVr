04c6fb4e36c8b8b26ab76247df792f278d0f0a04
JD <joeld@writeme.com>
2017-11-21 03:11:15 -0500

    undo if,then,else remove

--- rpi-kmod.sh
+++ rpi-kmod.sh
@@ -41,32 +41,28 @@ echo ""
 sleep 10
 clear
 echo "Now, the hard part.. Updating to the latest Raspbian Kernel version available."
-echo ""
+echo "=============================================================================="
 echo "Your current kernel version is : "`uname -r`
 echo "CPU_Architecture : "`arch`
-case $(arch) in
-  "armv61")
+if [ $(arch)="armv61" ]
+then
 echo "This is a Single-Core Proc. (armv61) , meaning a Raspberry Pi 0/0W/1/1B+/(orig.)2/A+/B+"
-  ;;
-  "armv71")
+else
 echo "This is a Quad-Core Proc. (armv71), meaning a Raspberry Pi 2/3/3B"
-  ;;
-esac
+fi
 export web_ver=" "
 export web_ver7=" "
 echo "Kernel Release no.: "$(uname -v|cut -b 1-6) $(echo "    pushed: -> ") $(uname -v|cut -b 10-17) $(uname -v|cut -b 30-35)
 echo "(Installed) Kernel hash : "$(vcgencmd version |grep 'version'|echo `cut -b 8-66`)
-case $(arch) in
-  "armv61")
+if [ $(arch)="armv61" ]
+then
 export web_ver=$(curl -s https://raw.githubusercontent.com/Hexxeh/rpi-firmware/master/uname_string)
 echo "Current release Version : "$(echo $web_ver|cut -b 14-22)$(echo $web_ver|cut -b 111-116) $(echo " (Available)  | You're on: "`uname -r`) $(echo "  "`uname -v|cut -b 1-6`)
-  ;;
-  "armv71")
+else
 export web_ver7=$(curl -s https://raw.githubusercontent.com/Hexxeh/rpi-firmware/master/uname_string7)
 echo "Current release Version : "$(echo $web_ver7|cut -b 14-25)$(echo $web_ver7|cut -b 114-120) $(echo " (Available)  | You're on: "`uname -r`) $(echo "  "`uname -v|cut -b 1-6`)
-  ;;
-esac
-echo ""
+fi
+echo "============================================================================"
 echo ""
 echo "!!!Be aware of the CPU architecture and the kernel version, it varies between"
 echo "Raspberry Pi types/models (i.e.: RPI 0/1/2/3)! --RPI single cores are [armv61]"
